53ca94c8fdc72705cee4e9375a2cf0af6c656072
Pheytunn <87560373+Pheytunn@users.noreply.github.com>
2021-11-10 05:06:37 -0800

    Add files via upload

---
+++ static/javascripts/uploadPage.js
@@ -0,0 +1,16 @@
+
+<script type="text/javascript">
+  JotForm.init(function(){
+if (window.JotForm && JotForm.accessible) $('input_9').setAttribute('tabindex',0);
+      JotForm.setCustomHint( 'input_9', 'Type here...' );
+  JotForm.newDefaultTheme = false;
+  JotForm.extendsNewTheme = false;
+  JotForm.newPaymentUIForNewCreatedForms = false;
+      JotForm.alterTexts({"confirmClearForm":"Are you sure you want to clear the form","lessThan":"Your score should be less than"});
+    /*INIT-END*/
+  });
+
+   JotForm.prepareCalculationsOnTheFly([null,{"name":"uploadPage","qid":"1","text":"Upload Page","type":"control_head"},{"name":"upload2","qid":"2","text":"Upload!","type":"control_button"},{"name":"uploadPhoto","qid":"3","text":"Upload Photo Here! :","type":"control_fileupload"},{"name":"nameOf","qid":"4","text":"Name of Region","type":"control_fullname"},null,null,null,null,{"name":"descriptionOf","qid":"9","subLabel":"Make","text":"Description of Photo","type":"control_textarea"}]);
+   setTimeout(function() {
+JotForm.paymentExtrasOnTheFly([null,{"name":"uploadPage","qid":"1","text":"Upload Page","type":"control_head"},{"name":"upload2","qid":"2","text":"Upload!","type":"control_button"},{"name":"uploadPhoto","qid":"3","text":"Upload Photo Here! :","type":"control_fileupload"},{"name":"nameOf","qid":"4","text":"Name of Region","type":"control_fullname"},null,null,null,null,{"name":"descriptionOf","qid":"9","subLabel":"Make","text":"Description of Photo","type":"control_textarea"}]);}, 20); 
+</script>
